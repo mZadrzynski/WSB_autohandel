c9099146f3c246a4c8973d476b562f394b0d2841
Marcin Zadrzynski <warshipdesign@gmail.com>
2022-06-01 15:54:43 +0200

    1.06.22 dodanie generatora aut

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,30 +1,56 @@
 package com.company;
+import java.util.Random;
+import java.text.DecimalFormat;
 
 public class Car {
     public String producer, color, carSegment;
-    public Integer mileage, price;
+    public Integer mileage;
+    public Double carPrice;
     public Boolean breaks, suspension, engine, gear, body;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
+    String producers[] = {"toyota", "mazda", "bmw", "mercedes", "fiat", "kia", "fiat", "wv", "audi", "ford"};
+    String colors[] = {"bialy", "szary", "czarny", "czerwony", "rozowy", "niebieski", "granatowy", "pomaranczowy", "zielony", "zolty"};
+    String carSegments[] = {"A", "B", "C", "D", "S", "dostawczy"};
+
 
     public Car(String carsName) {
-
-
-
-
+        Random r = new Random();
+        producer = producers[r.nextInt(0, 9)];
+        color = colors[r.nextInt(0, 9)];
+        carSegment = carSegments[r.nextInt(0, 4)];
+        mileage = r.nextInt(10000, 500000);
+        breaks = r.nextBoolean();
+        suspension = r.nextBoolean();
+        engine = r.nextBoolean();
+        gear = r.nextBoolean();
+        body = r.nextBoolean();
+        carPrice = 500000000.0 / mileage;
+        if (suspension || body && gear) {
+            carPrice *= 1.6;
+        }
+        if (producer == producers[2] || producer == producers[3]) {
+            carPrice *= 2.0;
+        }
+        if (carSegment == carSegments[4]) {
+            carPrice *= 2.5;
+        }
     }
 
-    @Override
-    public String toString() {
-        return "Car{" +
-                "producer='" + producer + '\'' +
-                ", color='" + color + '\'' +
-                ", carSegment='" + carSegment + '\'' +
-                ", mileage=" + mileage +
-                ", price=" + price +
-                ", breaks=" + breaks +
-                ", suspension=" + suspension +
-                ", engine=" + engine +
-                ", gear=" + gear +
-                ", body=" + body +
-                '}';
+        @Override
+        public String toString () {
+            return "Car " +
+                    "producer='" + producer + '\'' +
+                    ", color='" + color + '\'' +
+                    ", carSegment='" + carSegment + '\'' +
+                    ", mileage=" + mileage +
+                    ", price=" +  df.format(carPrice) +
+                    ", breaks=" + breaks +
+                    ", suspension=" + suspension +
+                    ", engine=" + engine +
+                    ", gear=" + gear +
+                    ", body=" + body;
+        }
     }
-}
+
+
--- src/com/company/Owner.java
+++ src/com/company/Owner.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Owner {
     public String name;
-    public Integer cash = 150000;
+    public Double cash = 150000.0;
     public Car car;
 
     public Owner() {
